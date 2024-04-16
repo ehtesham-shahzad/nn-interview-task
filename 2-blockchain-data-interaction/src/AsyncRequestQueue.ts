1e32ec6d55cddad11afa458823af0a3c8960cf1f
Ehtesham Shahzad <ehteshamshahzad@hotmail.com>
2024-04-17 04:25:18 +0500

    Minor updates

--- 2-blockchain-data-interaction/src/AsyncRequestQueue.ts
+++ 2-blockchain-data-interaction/src/AsyncRequestQueue.ts
@@ -5,7 +5,7 @@ export class AsyncRequestQueue {
   constructor() {
     this.queue = [];
     this.runningTask = 0;
-    this.maxConcurrentTasks = 4;
+    this.maxConcurrentTasks = 5;
   }
 
   enqueue<T>(promiseFactory: () => Promise<T>) {
